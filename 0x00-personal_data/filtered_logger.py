75763eb56544c7462affb2b4b783bd0b45bf034f
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-07-08 21:28:35 +0100

    :tada: "Added RedactingFormatter class to filtered_logger.py, modified filter_datum function to return a string, and added a test case at the end of the file."

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,10 @@
 # 1/usr/bin/env python3
 """ Filter Logger Module """
+import logging
 import re
 
 
-def filter_datum(fields: str, redaction: str, message: str, separator: str):
+def filter_datum(fields: str, redaction: str, message: str, separator: str) -> str:
     """
         Filter a message based on a set of fields and a redaction string
 
@@ -19,4 +20,40 @@ def filter_datum(fields: str, redaction: str, message: str, separator: str):
     """
     pattern = re.compile(
         rf'({"|".join(f"{field}=[^ {separator}]+" for field in fields)})')
-    return pattern.sub(lambda m: f"{m.group(0).split('=')[0]}={redaction}", message)
+    return pattern.sub(lambda m: f" {m.group(0).split('=')[0]}={redaction}", message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+            Format a log record
+
+            Args:
+                record (logging.LogRecord): The log record to format
+
+            Returns:
+                str: The formatted log record
+        """
+        original_message = super().format(record)
+        return filter_datum(
+            self.fields, self.REDACTION, original_message,
+            self.SEPARATOR
+        )
+    
+
+message = "name=Bob;email=bob@dylan.com;ssn=000-123-0000;password=bobby2019;"
+log_record = logging.LogRecord(
+    "my_logger", logging.INFO, None, None, message, None, None)
+formatter = RedactingFormatter(fields=("email", "ssn", "password"))
+print(formatter.format(log_record))
