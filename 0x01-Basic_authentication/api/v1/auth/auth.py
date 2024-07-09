786cb0fd748db852c7a9f481896d3eb5a8f96ba4
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-07-09 21:55:07 +0100

    :shirt: "Removed test code from Auth class and fixed indentation in require_auth method."

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -13,7 +13,7 @@ class Auth:
             return True
         if excluded_paths is None or len(excluded_paths) == 0:
             return True
-        
+
         # Ensure path ends with a slash for comparison
         if not path.endswith('/'):
             path += '/'
@@ -30,14 +30,3 @@ class Auth:
     def current_user(self, request=None) -> TypeVar:
         """ Current user """
         return None
-
-
-a = Auth()
-
-print(a.require_auth(None, None))
-print(a.require_auth(None, []))
-print(a.require_auth("/api/v1/status/", []))
-print(a.require_auth("/api/v1/status/", ["/api/v1/status/"]))
-print(a.require_auth("/api/v1/status", ["/api/v1/status/"]))
-print(a.require_auth("/api/v1/users", ["/api/v1/status/"]))
-print(a.require_auth("/api/v1/users", ["/api/v1/status/", "/api/v1/stats"]))
