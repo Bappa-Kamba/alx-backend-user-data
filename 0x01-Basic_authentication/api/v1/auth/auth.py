5ba8ea6de4fc63cbcef5921d0339fe99186ee8ad
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-07-09 21:50:11 +0100

    :zap: "Updated require_auth method in Auth class to handle excluded paths and path endings"

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,12 +9,18 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: list = []) -> bool:
         """ Require auth """
-        if path not in excluded_paths:
-            return True
         if path is None:
             return True
-        if excluded_paths is None or excluded_paths == []:
+        if excluded_paths is None or len(excluded_paths) == 0:
             return True
+        
+        # Ensure path ends with a slash for comparison
+        if not path.endswith('/'):
+            path += '/'
+        # Check if the path is in the list of excluded paths
+        for excluded_path in excluded_paths:
+            if not (excluded_path.endswith('/') and path == excluded_path):
+                return True
         return False
 
     def authorization_header(self, request=None) -> str:
