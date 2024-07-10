28289650c66c8f8f1ea1f8ae0150d984032ef7a3
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-07-10 16:41:13 +0100

    ;zap: "Added import of base64 module and new method decode_base64_authorization_header to BasicAuth class"

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Basic Auth Module """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -19,3 +20,32 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str
+    ) -> str:
+        """ Decodes base64 authorization header """
+        if not base64_authorization_header or \
+                type(base64_authorization_header) != str:
+            return None
+        try:
+            decoded_str = base64.b64decode(
+                base64_authorization_header,
+                validate=True
+            )
+        except Exception:
+            return None
+
+        return decoded_str.decode('UTF-8')
+
+
+a = BasicAuth()
+
+print(a.decode_base64_authorization_header(None))
+print(a.decode_base64_authorization_header(89))
+print(a.decode_base64_authorization_header("Holberton School"))
+print(a.decode_base64_authorization_header("SG9sYmVydG9u"))
+print(a.decode_base64_authorization_header("SG9sYmVydG9uIFNjaG9vbA=="))
+print(a.decode_base64_authorization_header(
+    a.extract_base64_authorization_header("Basic SG9sYmVydG9uIFNjaG9vbA==")))
