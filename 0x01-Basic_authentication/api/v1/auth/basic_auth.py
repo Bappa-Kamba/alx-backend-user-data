6b5eed6f24fb8553b6a07fb1c1502a8ddbe1e96c
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-07-10 05:41:50 +0100

    :zap: "Added extract_base64_authorization_header method to BasicAuth class and test cases"

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,3 +7,26 @@ class BasicAuth(Auth):
     """ Basic Auth class """
     def __init__(self) -> None:
         super().__init__()
+
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str
+        ) -> str:
+        """ Extracts base64 authorization header """
+        if not authorization_header and type(authorization_header) != str:
+            return None
+        if not authorization_header.startswith('Basic'):
+            return None
+        return authorization_header[6:]
+    
+
+a = BasicAuth()
+
+print(a.extract_base64_authorization_header(None))
+print(a.extract_base64_authorization_header(89))
+print(a.extract_base64_authorization_header("Holberton School"))
+print(a.extract_base64_authorization_header("Basic Holberton"))
+print(a.extract_base64_authorization_header("Basic SG9sYmVydG9u"))
+print(a.extract_base64_authorization_header("Basic SG9sYmVydG9uIFNjaG9vbA=="))
+print(a.extract_base64_authorization_header("Basic1234"))
+        
