f98af91c655e10c690c046cfdeddf7557ddcffc5
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-07-11 14:02:17 +0100

    :fire: "Remove test code from BasicAuth class"

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,8 +6,6 @@ from typing import Tuple
 from models.user import User
 
 
-
-
 class BasicAuth(Auth):
     """ Basic Auth class """
 
@@ -67,7 +65,7 @@ class BasicAuth(Auth):
         """ Creates user object from user credentials """
         if user_email is None or type(user_email) != str:
             return None
-        
+
         if user_pwd is None or type(user_pwd) != str:
             return None
         curr_user = User.search({'email': user_email})
@@ -78,36 +76,3 @@ class BasicAuth(Auth):
         if not valid_password:
             return None
         return curr_user[0]
-
-import uuid
-from models.user import User
-
-""" Create a user test """
-user_email = str(uuid.uuid4())
-user_clear_pwd = str(uuid.uuid4())
-user = User()
-user.email = user_email
-user.first_name = "Bob"
-user.last_name = "Dylan"
-user.password = user_clear_pwd
-print("New user: {}".format(user.display_name()))
-user.save()
-
-""" Retreive this user via the class BasicAuth """
-
-a = BasicAuth()
-
-u = a.user_object_from_credentials(None, None)
-print(u.display_name() if u is not None else "None")
-
-u = a.user_object_from_credentials(89, 98)
-print(u.display_name() if u is not None else "None")
-
-u = a.user_object_from_credentials("email@notfound.com", "pwd")
-print(u.display_name() if u is not None else "None")
-
-u = a.user_object_from_credentials(user_email, "pwd")
-print(u.display_name() if u is not None else "None")
-
-u = a.user_object_from_credentials(user_email, user_clear_pwd)
-print(u.display_name() if u is not None else "None")
