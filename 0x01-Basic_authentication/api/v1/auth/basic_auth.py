038f45bdd2cad627a27ee37e3a5e68e90c2382b0
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-07-11 23:31:36 +0100

    final test

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -85,3 +85,6 @@ ba = BasicAuth()
 res = ba.user_object_from_credentials("u1@gmail.com", "pwd")
 if res is not None:
     print("user_object_from_credentials must return None if 'user_email' is not linked to any user")
+
+
+print('OK')
