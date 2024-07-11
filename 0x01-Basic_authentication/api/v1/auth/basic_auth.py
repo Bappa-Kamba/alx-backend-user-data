95f36d99857b2cd7536d24f3a94e04aeb3fc1903
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-07-11 13:49:39 +0100

    :zap: "Added user_object_from_credentials method to BasicAuth class and test code to create a user and retrieve it via BasicAuth"

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,7 +2,8 @@
 """ Basic Auth Module """
 from api.v1.auth.auth import Auth
 import base64
-from typing import Tuple
+from typing import Tuple, TypeVar
+
 
 
 class BasicAuth(Auth):
@@ -55,3 +56,57 @@ class BasicAuth(Auth):
             return tuple(decoded_base64_authorization_header.split(':'))
         else:
             return (None, None)
+
+    def user_object_from_credentials(
+            self,
+            user_email: str,
+            user_pwd: str
+    ) -> TypeVar['User']:
+        """ Creates user object from user credentials """
+        from models.user import User
+        if user_email is None or type(user_email) != str:
+            return None
+        
+        if user_pwd is None or type(user_pwd) != str:
+            return None
+        curr_user = User.search({'email': user_email})[0]
+        if not curr_user:
+            return None
+        valid_password = curr_user.is_valid_password(user_pwd)
+
+        if not valid_password:
+            return None
+        return curr_user
+
+import uuid
+from models.user import User
+
+""" Create a user test """
+user_email = str(uuid.uuid4())
+user_clear_pwd = str(uuid.uuid4())
+user = User()
+user.email = user_email
+user.first_name = "Bob"
+user.last_name = "Dylan"
+user.password = user_clear_pwd
+print("New user: {}".format(user.display_name()))
+user.save()
+
+""" Retreive this user via the class BasicAuth """
+
+a = BasicAuth()
+
+u = a.user_object_from_credentials(None, None)
+print(u.display_name() if u is not None else "None")
+
+u = a.user_object_from_credentials(89, 98)
+print(u.display_name() if u is not None else "None")
+
+u = a.user_object_from_credentials("email@notfound.com", "pwd")
+print(u.display_name() if u is not None else "None")
+
+u = a.user_object_from_credentials(user_email, "pwd")
+print(u.display_name() if u is not None else "None")
+
+u = a.user_object_from_credentials(user_email, user_clear_pwd)
+print(u.display_name() if u is not None else "None")
