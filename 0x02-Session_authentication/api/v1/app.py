fdd5cfbf3c29afc9c1804da4ec746341157bca77
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-07-12 21:38:15 +0100

    :zap: "Added support for 'session_auth' authentication type and refactored AUTH_TYPE environment variable access"

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -6,19 +6,21 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
-
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-if getenv('AUTH_TYPE') == 'auth':
+auth_type = getenv('Auth_TYPE')
+if auth_type == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
-elif getenv('AUTH_TYPE') == 'basic_auth':
+elif auth_type == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+elif auth_type == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 
 
 @app.before_request
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Session Auth Module """
+from auth import Auth
+
+
+class SessioAuth(Auth):
+    """ Session Auth Class """
+    pass
