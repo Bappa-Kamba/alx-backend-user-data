e7b96b46c03dd2a2cef2c2145ded37bcd85027b1
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-07-12 22:04:27 +0100

    :zap: "Updated condition in create_session method to check if user_id is falsy or not a string"

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -14,7 +14,7 @@ class SessionAuth(Auth):
 
     def create_session(self, user_id: str = None) -> str:
         """ Creates a Session ID for a user_id """
-        if not user_id and type(user_id) != str:
+        if not user_id or type(user_id) != str:
             return None
         sess_id = str(uuid.uuid4())
         self.user_id_by_session_id[sess_id] = user_id
