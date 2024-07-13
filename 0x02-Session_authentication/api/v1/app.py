72221092fa8787668dc307c0ce6d06e1797e6e44
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-07-13 01:27:10 +0100

    :zap: "Updated before_request function to check for both authorization header and session cookie, and added new routes to exemption list."

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -32,9 +32,14 @@ def before_request():
             [
                 '/api/v1/status/',
                 '/api/v1/unauthorized/',
-                '/api/v1/forbidden/']
+                '/api/v1/forbidden/',
+                '/api/v1/auth_session/login/',
+            ]
         ):
-            if not auth.authorization_header(request):
+            if not (
+                auth.authorization_header(request) and \
+                auth.session_cookie(request)
+            ):
                 abort(401)
             curr_user = auth.current_user(request)
             if not curr_user:
