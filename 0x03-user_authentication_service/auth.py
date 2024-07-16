c412b8c1a13d009b3e80ec25695696388db6c6f0
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-07-16 23:00:56 +0100

    :zap: created auth.py and implemented _hash_password to has passwords

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" Authorization Module"""
+import bcrypt
+from typing import ByteString
+
+
+def _hash_password(password: str) -> ByteString:
+    """
+        Method to hash password string
+
+        Args:
+            password: the password string to hash
+
+        Returns:
+            a byte string - a salted hash password of `password`
+    """
+    return bcrypt.hashpw(password)
+
+
+print(_hash_password("Hello Holberton"))
