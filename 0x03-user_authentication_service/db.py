3394e3d9d553a1de0676b26a9a01358d1bdb3490
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-07-16 22:25:23 +0100

    :fire: "Removed redundant import, merged into single line"

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -3,8 +3,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError, NoResultFound
 from user import User, Base
 
 
