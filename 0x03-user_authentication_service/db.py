f2bb7ae1f0db446366c89bec1e749f0c3a75b519
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-07-16 22:03:11 +0100

    :fire: "Remove test code from DB class

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -42,12 +42,3 @@ class DB:
         self._session.commit()
 
         return user
-
-
-my_db = DB()
-
-user_1 = my_db.add_user("test@test.com", "SuperHashedPwd")
-print(user_1.id)
-
-user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
-print(user_2.id)
