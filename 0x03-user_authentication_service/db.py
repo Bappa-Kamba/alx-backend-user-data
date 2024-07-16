a0f63cdb4d8d550fbd85f8513470696d1e75ec64
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-07-16 22:20:12 +0100

    :fire: "Removed test code from DB class implementation"

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -7,6 +7,7 @@ from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
 from user import User, Base
 
+
 class DB:
     """ DB Class """
 
@@ -40,7 +41,7 @@ class DB:
         self._session.commit()
 
         return user
-    
+
     def find_user_by(self, **kwargs):
         """
             Finds a user by key word arguments
@@ -61,24 +62,3 @@ class DB:
             raise NoResultFound('Not found')
         except InvalidRequestError:
             raise InvalidRequestError('Invalid')
-
-
-my_db = DB()
-
-user = my_db.add_user("test@test.com", "PwdHashed")
-print(user.id)
-
-find_user = my_db.find_user_by(email="test@test.com")
-print(find_user.id)
-
-try:
-    find_user = my_db.find_user_by(email="test2@test.com")
-    print(find_user.id)
-except NoResultFound:
-    print("Not found")
-
-try:
-    find_user = my_db.find_user_by(no_email="test@test.com")
-    print(find_user.id)
-except InvalidRequestError:
-    print("Invalid")
