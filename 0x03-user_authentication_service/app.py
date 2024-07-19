1b082eb367e659b89f18890d4a8a9c54ab234dde
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-07-19 21:35:48 +0100

    :zap: "Added login route to app.py, including validation and session creation"

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ Flask Module """
-from flask import jsonify, Flask, request
+from flask import jsonify, Flask, request, abort
 from auth import Auth
 
 AUTH = Auth()
@@ -26,6 +26,20 @@ def users():
         return jsonify({
             "email": f"{user.email}", "message": "user created"
         }), 200
+
+
+@app.route('/sessions', methods=["POST"])
+def login():
+    """ login route """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    session_id = AUTH.create_session(email)
+    resp = jsonify({"email": f"{email}", "message": "logged in"})
+    resp.set_cookie('session_id', session_id)
+    return resp
 
 
 if __name__ == "__main__":
