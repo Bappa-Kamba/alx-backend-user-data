ef17961ed344accf64db5d80af03e05b82cccfc2
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-07-19 20:26:16 +0100

    :zap: "Removed test code for registering a user and added a call to print the hashed password of 'Hello Holberton'"

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -45,20 +45,5 @@ class Auth:
             new_user = self.register_user(email, hashed_password)
             return new_user
 
-
-email = 'me@me.com'
-password = 'mySecuredPwd'
-
 auth = Auth()
-
-try:
-    user = auth.register_user(email, password)
-    print("successfully created a new user!")
-except ValueError as err:
-    print("could not create a new user: {}".format(err))
-
-try:
-    user = auth.register_user(email, password)
-    print("successfully created a new user!")
-except ValueError as err:
-    print("could not create a new user: {}".format(err))
+print(auth._hash_password("Hello Holberton"))
