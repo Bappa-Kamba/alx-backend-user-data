f142fdad3b472e968b9cc262edf0ccb5c5d6d36d
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-07-19 23:05:15 +0100

    :zap: "Swap order of exception handling blocks for ValueError and NoResultFound in Auth class"

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -141,9 +141,9 @@ class Auth:
             user = self._db.find_user_by(email=email)
             reset_token = _generate_uuid()
             self._db.update_user(user.id, reset_token=reset_token)
-        except ValueError:
-            raise ValueError('Invalid attribute')
         except NoResultFound:
             raise NoResultFound('No user found')
+        except ValueError:
+            raise ValueError('Invalid attribute')
         else:
             return reset_token
