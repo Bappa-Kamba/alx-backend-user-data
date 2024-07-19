8db039d077a0feb2a2e0d496d638a14b8297a4d1
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-07-19 20:47:32 +0100

    :zap: "Added user registration endpoint and imported Auth class"

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """ Flask Module """
-from flask import jsonify, Flask
+from flask import jsonify, Flask, request
+from auth import Auth
 
-
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -10,6 +11,21 @@ app = Flask(__name__)
 def index():
     """ index route """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=["POST"])
+def users():
+    """ users route """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    else:
+        return jsonify({
+            "email": f"{user.email}", "message": "user created"
+        }), 200
 
 
 if __name__ == "__main__":
