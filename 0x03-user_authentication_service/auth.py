f9d283d8a620f7a6ae4feab37bd99d9fa6a3b735
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-07-19 20:35:58 +0100

    :zap: "Replace self.register_user with self._db.add_user in Auth class"

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -42,5 +42,5 @@ class Auth:
             raise ValueError(f'User {email} already exists')
         except NoResultFound:
             hashed_password = _hash_password(password)
-            new_user = self.register_user(email, hashed_password)
+            new_user = self._db.add_user(email, hashed_password)
             return new_user
