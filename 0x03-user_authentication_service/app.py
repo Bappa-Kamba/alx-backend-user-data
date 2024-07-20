5e07f374c22f9f58199177a59e1301a7ac774b39
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-07-20 13:02:17 +0100

    :books: "Updated docstring for update_password function"

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -88,7 +88,7 @@ def get_reset_password_token():
 
 @app.route('/reset_password', methods=["PUT"])
 def update_password():
-    """ update password """
+    """ update password route """
     email = request.form.get('email')
     new_password = request.form.get('new_password')
     reset_token = request.form.get('reset_token')
