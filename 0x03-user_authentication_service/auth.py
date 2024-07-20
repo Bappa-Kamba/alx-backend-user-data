6182caea030245383baf893c7c8bcf1287e555cf
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-07-20 12:37:29 +0100

    :zap: "Update update_user method call to use 'hashed_password' keyword argument instead of 'password'"

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -157,7 +157,7 @@ class Auth:
             user = self._db.find_user_by(reset_token=reset_token)
             hashed_password = _hash_password(password)
             self._db.update_user(
-                user.id, password=hashed_password,
+                user.id, hashed_password=hashed_password,
                 reset_token=None
             )
         except NoResultFound:
