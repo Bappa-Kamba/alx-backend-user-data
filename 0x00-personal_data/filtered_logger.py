424eba6da77bfb5e4ad77df2993e55589bb73970
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-07-22 11:01:26 +0100

    :zap: used re.sub

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,8 +4,20 @@ import re
 
 
 def filter_datum(fields, redaction, message, separator):
-    pattern = re.compile(
-        rf'({"|".join(f"{field}=[^ {separator}]+" for field in fields)})')
-    return pattern.sub(
-        lambda m: f"{m.group(0).split('=')[0]}={redaction}", message
+    """
+        Function to filter and redact records.
+
+        Args:
+            fields: fields to redact
+            redaction: redaction string
+            message: message that contains fields to redact
+            separator: string separating the fields
+
+        Returns:
+            redacted string
+    """
+    return re.sub(
+        rf'({"|".join(f"{field}=[^ {separator}]+" for field in fields)})',
+        lambda m: f"{m.group(0).split('=')[0]}={redaction}",
+        message
     )
