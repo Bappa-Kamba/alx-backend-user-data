b5b8bda3e054b9253ce5a3123f425271d3ceecc1
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-07-23 22:29:59 +0100

    :zap: "Added is_valid function to check if a hashed password is valid"

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -11,3 +11,8 @@ def hash_password(password: str) -> bytes:
     )
 
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str):
+    """ Check if a hashed password is valid. """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
